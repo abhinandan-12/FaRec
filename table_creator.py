85c786b83ad78e006054db1ee66d82bf3e364654
Abhinandan Mandal <42448130+abhinandan-afrs@users.noreply.github.com>
2020-01-29 20:44:39 +0530

    Update table_creator.py

--- table_creator.py
+++ table_creator.py
@@ -1,25 +1,46 @@
-def table_crt():
+def db_crt():
     import mysql.connector as sq
     import time as t
     db=sq.connect(host='localhost',user='root',passwd='1234')
+    cursor=db.cursor()
     print("Checking Database")
-    print('Loading',end="")
-    for i in range(0,20):
-        print(".",end="")
-        t.sleep(0.01)
-    qu='create database farec;'
-    try:
+    check='show databases'
+    cursor.execute(check)
+    l=cursor.fetchall()
+    l=[i[0] for i in l]
+    if not 'farec' in l:
+        qu='create database farec;'
+        print("Database doesn't exist\nCreating database")
         cursor.execute(qu)
-    except:
-        t.sleep(0.5)
-        print("\nDatabase  found!\nChecking Table")
         print('Loading',end="")
         for i in range(0,20):
             print(".",end="")
             t.sleep(0.01)
-        qu1='create table record (roll int(11), name varchar(20), adm int(11), farec_id int(11) PRIMARY KEY);'
-        try:
-            cursor.execute(qu1)
-        except:
+        db.commit()
+        db.close()
+    else:
+        print("\nDatabase found!")
+
+def tb_crt():
+    import mysql.connector as sq
+    import time as t
+    tb=sq.connect(host='localhost',user='root',passwd='1234',database='farec')
+    crsr=tb.cursor()
+    print("\nChecking Table")
+    check2='show tables'
+    crsr.execute(check2)
+    m=crsr.fetchall()
+    m=[i[0] for i in m]
+    if not 'record' in m:
+        qu2='create table record (roll int(11), name varchar(20), adm int(11), farec_id int(11) PRIMARY KEY);'
+        print("Table doesn't exist\nCreating table")
+        crsr.execute(qu2)
+        print('Loading',end="")
+        for i in range(0,20):
+            print(".",end="")
             t.sleep(0.01)
-            print("\nTable found!\n------------")
+        print("\n")
+        tb.commit()
+        tb.close()
+    else:
+        print("\nTable found!\n------------\n")
