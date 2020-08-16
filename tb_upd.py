15288d8e55288528562678553867a1612ac8d780
Abhinandan Mandal <42448130+abhinandan-afrs@users.noreply.github.com>
2020-08-16 21:03:38 +0530

    Update tb_upd.py

--- tb_upd.py
+++ tb_upd.py
@@ -1,13 +1,16 @@
-import mysql.connector as sq
-from datetime import datetime
+def tb_upd():
+    import mysql.connector as sq
+    from datetime import datetime
 
-db=sq.connect(host='localhost',user='root',passwd='1234',database='farec')
-cursor=db.cursor()
+    db=sq.connect(host='localhost',user='root',passwd='1234',database='farec')
+    cursor=db.cursor()
 
-now=datetime.now()
-dt=now.strftime("%d_%m_%Y")
-qu="alter table record add "+dt+" varchar(3) default 'no';"
-cursor.execute(qu)
-print(dt)
-cursor.close()
-db.close()
+    now=datetime.now()
+    dt=now.strftime("%d_%m_%Y")
+    qu="alter table record add "+dt+" varchar(3) default 'no';"
+    cursor.execute(qu)
+    cursor.close()
+    db.close()
+    print(dt)
+    print("Done!")
+tb_upd()
