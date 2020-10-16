eeb3024030bc2e681f9ce326b9e1b35e93ad12f7
Abhinandan Mandal <42448130+abhinandan-afrs@users.noreply.github.com>
2020-10-16 23:24:32 +0530

    Update data_col.py

--- data_col.py
+++ data_col.py
@@ -16,7 +16,6 @@ cam.set(4, 480) # set video height
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# For each person, enter one numeric face id (must enter number start from 1, this is the lable of person 1)
 face_id = input('\n Enter your FaRec ID: ')
 
 que='select name from record where farec_id ={};'.format(face_id)
@@ -26,8 +25,7 @@ for i in name_list:
     name=i
 print(name)
 
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-# Initialize individual sampling face count
+print("\nInitializing face capture. Look the camera and wait ...")
 count = 0
 
 # Start detect your face and take 30 pictures
@@ -42,7 +40,6 @@ while(True):
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
 
-        # Save the captured image into the datasets folder
         cv2.imwrite("dataset/User." + str(face_id)  + '.' + str(name) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
 
         cv2.imshow('image', img)
@@ -50,11 +47,10 @@ while(True):
     k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
     if k == 27:
         break
-    elif count >= 30: # Take 30 face sample and stop video
+    elif count >= 30:
          break
 
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
+print("\nExiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
 
